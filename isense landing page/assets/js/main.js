20b95c9dbc266b3b1ecbf61229be0252dc2a6d93
pythagoras-dev <58697223+pythagoras-dev@users.noreply.github.com>
2020-07-27 11:19:04 +0100

    changed somethings

---
+++ isense landing page/assets/js/main.js
@@ -0,0 +1,129 @@
+//Modal images
+function preloader() {
+    var preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    preloader.setAttribute("aria-busy", "false");
+    document.getElementById("site").style.opacity = "1";
+}
+window.onload = preloader;
+// Lets submit the subscribe form
+$("#subscriber-form").on("submit", function(e) {
+    e.preventDefault();
+
+    $.post("subscribe.php", $("#subscriber-form").serialize(), function(data) {
+        const res = JSON.parse(data);
+
+        if (res.signal === "ok") {
+            toastr.success(res.msg);
+            $("#msg").hide();
+            $("#subscriber-form input").val(function() {
+                return "";
+            });
+        } else {
+            toastr.error(res.msg);
+        }
+    });
+});
+
+$("#subscriber-form2").on("submit", function(e) {
+    e.preventDefault();
+
+    $.post("subscribe.php", $("#subscriber-form2").serialize(), function(data) {
+        const res = JSON.parse(data);
+
+        if (res.signal === "ok") {
+            toastr.success(res.msg);
+            $("#msg").hide();
+            $("#subscriber-form2 input").val(function() {
+                return "";
+            });
+        } else {
+            toastr.error(res.msg);
+        }
+    });
+});
+
+////////////////////////////////////////////
+$(".carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    dots: true,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
+
+$(".carousel-iframe").magnificPopup({
+    type: "iframe",
+});
+
+$(".carousel").magnificPopup({
+    delegate: ".carousel-link",
+    gallery: {
+        enabled: true,
+    },
+    type: "image",
+    callbacks: {
+        elementParse: function(item) {
+            if (item.el[0].classList.contains("video-link")) {
+                item.type = "iframe";
+            } else {
+                item.type = "image";
+            }
+        },
+    },
+});
+
+////////////////////////////////////////////
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
+
+///////////////////////////////////
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
--- isense landing page/subscribe.php
+++ isense landing page/subscribe.php
@@ -2,23 +2,28 @@
 
 include './database.php';
 
-$email = trim($_POST['email']);  // To trim white spaces
 
-$email = stripslashes($email); // to remove back slashes
+//Trim white spaces
+$email = trim($_POST['email']);
 
-$email = htmlspecialchars($email); // to remove special characters
+//Remove back slashes
+$email = stripslashes($email);
+
+//Remove special characters
+$email = htmlspecialchars($email);
 
 $result = mysqli_num_rows(mysqli_query($conn, "SELECT 1 FROM early_access WHERE email = '$email';"));
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Check if email is valid
-{
+
+// Check if email is valid
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $signal = 'bad';
     $message = 'Please enter a valid email';
 } elseif ($result > 0) // Check if email exists already
 {
     $signal = 'bad';
     $message = 'Email already exists';
-} else { // All is good.. Lets save the email
+} else {
     $sql = "INSERT INTO `early_access` (`email`) VALUES ('$email')";
 
     if (mysqli_query($conn, $sql)) {
@@ -30,7 +35,8 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Check if email is valid
     }
 }
 
-mysqli_close($conn); // Close connection
+// Close connection
+mysqli_close($conn);
 
 $data = array(
     'signal' => $signal,
